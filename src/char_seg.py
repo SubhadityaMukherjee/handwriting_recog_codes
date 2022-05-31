1350e8c583e50fb1279c8467e3ac7b8662e75662
Paul0808 <aqwsecret@yahoo.com>
2022-05-31 19:22:15 +0200

    more fine tuning char segmentation

--- src/char_seg.py
+++ src/char_seg.py
@@ -25,24 +25,57 @@ def mergeBoxes(box1, box2):
     return holder
 
 
-def cleanBoxes(box, bbox):
-    # Bounding box is saved only if it is 10 pixels or bigger
-    # This eliminates small bounding boxes on noise
-    if (box[2] < 15) or (box[3] < 15):
+def checkMerge(box1, box2):
+    # Following lines merge two bounding boxes, if box1 is inside box2.
+    # A leeway is given, so box1 can be 70% of its width outside box2 (on the x axis)
+    if ((box2[0] - box1[2] * 7 / 10) <= box1[0]) and \
+            ((box2[0] + box2[2] + box1[2] * 7 / 10) >= (box1[0] + box1[2])):
+        # Following if statement is for the y axis. On this axis, if at least one of the edges of the current
+        # bounding box is inside the previous bounding box the boxes will be merged.
+        if (box2[1] < (box1[1] + box1[3]) <= (box2[1] + box2[3])) or \
+                ((box2[1] + box2[3]) > box1[1] >= box2[1]):
+            box1 = mergeBoxes(box1, box2)
+            return box1
+    else:
         return
-    # 1 character is somewhere between 25 pixels and 75 pixels in width
+
+
+def cleanBoxes(box, bbox):
+    # Bounding box is saved only if it is 25 pixels or bigger
+    # This eliminates small bounding boxes on noise
+    if (box[2] < 25) or (box[3] < 25):
+        return
+    # 1 character is somewhere between 25-40 pixels and 75 pixels in width
     # These characters are directly returned
-    elif (box[2] > 25) and (box[2] < 75):
+    elif (box[2] > 45) and (box[2] < 75):
         return box
     # Taking care of connected components
     # TODO: find a way to split components
     elif box[2] > 75:
         return box
     else:
+        if len(bbox) >= 2:
+            hold = checkMerge(bbox[-2], box)
+            if hold:
+                bbox.pop()
+                bbox.append(hold)
+                print(hold)
+
+        if len(bbox) >= 1:
+            hold = checkMerge(box, bbox[-1])
+            if hold:
+                bbox.pop()
+                bbox.append(hold)
+                print(hold)
+            hold = checkMerge(bbox[-1], box)
+            if hold:
+                bbox.pop()
+                bbox.append(hold)
+                print(hold)
         # Following lines merge two bounding boxes, if the current bounding box is inside the previous registered
         # bounding box. A leeway is given, so the current bounding bounding box can be 70% of its width outside the
         # previous bounding box (on the x axis)
-        if bbox and ((bbox[-1][0] - box[2]*7/10) <= box[0]) and\
+        """if bbox and ((bbox[-1][0] - box[2]*7/10) <= box[0]) and\
                 ((bbox[-1][0] + bbox[-1][2] + box[2]*7/10) >= (box[0] + box[2])):
             # Following if statement is for the y axis. On this axis, if at least one of the edges of the current
             # bounding box is inside the previous bounding box the boxes will be merged.
@@ -59,7 +92,7 @@ def cleanBoxes(box, bbox):
                     ((box[1] + box[3]) > bbox[1] >= box[1]):
                 box = mergeBoxes(bbox[-1], box)
                 bbox.pop()
-                print(box)
+                print(box)"""
         return box
 
 
