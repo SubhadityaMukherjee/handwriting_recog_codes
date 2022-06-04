a6c22daadee1c3c0361b2d14372be2a000708b08
Paul0808 <aqwsecret@yahoo.com>
2022-06-04 16:52:49 +0200

    Update char_seg.py

--- src/char_seg.py
+++ src/char_seg.py
@@ -153,15 +153,14 @@ def splitConnectedElements(image, box):
 def cleanBoxes(image, box, bbox):
     # Bounding box is saved only if it is 15 pixels or bigger
     # This eliminates small bounding boxes on noise
-    if (box[2] < 20) or (box[3] < 20):
+    if (box[2] < 25) or (box[3] < 25):
         return
     # 1 character is somewhere between 25-40 pixels and 75 pixels in width
     # These characters are directly returned
-    elif (box[2] > 40) and (box[2] < 95):
+    elif (box[2] > 40) and (box[2]/box[3] < 1.2):
         return box
     # Taking care of connected components
-    # TODO: find a way to split components
-    elif box[2] > 94:
+    elif box[2]/box[3] >= 1.2:
         box = splitConnectedElements(image, box)
         return box
     else:
@@ -186,24 +185,6 @@ def cleanBoxes(image, box, bbox):
         # Following lines merge two bounding boxes, if the current bounding box is inside the previous registered
         # bounding box. A leeway is given, so the current bounding bounding box can be 70% of its width outside the
         # previous bounding box (on the x axis)
-        """if bbox and ((bbox[-1][0] - box[2]*7/10) <= box[0]) and\
-                ((bbox[-1][0] + bbox[-1][2] + box[2]*7/10) >= (box[0] + box[2])):
-            # Following if statement is for the y axis. On this axis, if at least one of the edges of the current
-            # bounding box is inside the previous bounding box the boxes will be merged.
-            if (bbox[-1][1] < (box[1] + box[3]) <= (bbox[-1][1] + bbox[-1][3])) or\
-                    ((bbox[-1][1] + bbox[-1][3]) > box[1] >= bbox[-1][1]):
-                box = mergeBoxes(bbox[-1], box)
-                bbox.pop()
-                print(box)
-        # Following elif statement is the opposite of the last if statement. it does the same merging, but it checks if
-        # the previous bounding box is inside the current one
-        elif bbox and ((box[0] - bbox[-1][2]*7/10) <= bbox[-1][0]) and\
-                ((box[0] + box[2] + bbox[-1][2]*7/10) >= (bbox[-1][0] + bbox[-1][2])):
-            if (box[1] < (bbox[-1][1] + bbox[-1][3]) <= (box[1] + box[3])) or \
-                    ((box[1] + box[3]) > bbox[1] >= box[1]):
-                box = mergeBoxes(bbox[-1], box)
-                bbox.pop()
-                print(box)"""
         return box
 
 
